cbc96357a8eee79f5ea7a1d5c1b927bbda879e88
xlzy <1017866168@qq.com>
2023-07-17 17:33:32 +0800

    完成基本功能

--- app.js
+++ app.js
@@ -12,15 +12,16 @@ app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 app.post('/webhook', (req, res) => {
   // 检查事件类型是否为 push
-  console.log(req.headers, '===========打印的 ------ req.headers');
+  console.log('x-github-event:', req.headers['x-github-event']);
   if (req.headers['x-github-event'] === 'push') {
     // 获取仓库信息
     const repository = req.body.repository;
-    console.log(repository, '===========打印的 ------ ');
     const repoName = repository.name;
     const repoURL = repository.clone_url;
+    console.log('repoName: ',repoName);
+    console.log('repoURL: ',repoURL);
     // 需要先删除本地仓库，否则 clone 会失败
-    const rmProcess = spawn('rm', ['-rf', `./${repoName}`]);
+    const rmProcess = spawn('rm', ['-rf', `../${repoName}`]);
     rmProcess.on('close', (code) => {
       if (code === 0) {
         console.log('Delete successful');
@@ -28,7 +29,7 @@ app.post('/webhook', (req, res) => {
         const cloneProcess = spawn('git', [
           'clone',
           repoURL,
-          `./${repoName}`,
+          `../${repoName}`,
         ]);
         
         cloneProcess.on('close', (code) => {
@@ -46,24 +47,6 @@ app.post('/webhook', (req, res) => {
       }
     })
   }
-});
-
-app.get('/webhook_test', (req, res) => {
-  const repoURL = 'https://github.com/dashjidasnda/yusa.cc.git';
-  const repoName = repoURL.split('/').pop().split('.')[0];
-
-  // 执行 git clone 命令
-  const cloneProcess = spawn('git', ['clone', repoURL, `./${repoName}`]);
-
-  cloneProcess.on('close', (code) => {
-    if (code === 0) {
-      console.log('Clone successful');
-      // 执行其他操作，例如重启服务器、更新数据库等
-    } else {
-      console.error(`Clone failed with code ${code}`);
-    }
-  });
-  res.sendStatus(200);
 });
 
 app.use((err, req, res, next) => {
