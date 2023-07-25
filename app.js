2c7b7c8d39f71238e833a149e44a7def5d887be8
XLZY <1017866168@qq.com>
2023-07-25 23:00:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { spawn } = require('child_process');
+const fs = require('fs');
+const { exec } = require('child_process');
 
 const app = express();
 
@@ -18,35 +19,114 @@ app.post('/webhook', (req, res) => {
     const repository = req.body.repository;
     const repoName = repository.name;
     const repoURL = repository.clone_url;
-    console.log('repoName: ',repoName);
-    console.log('repoURL: ',repoURL);
-    // 需要先删除本地仓库，否则 clone 会失败
-    const rmProcess = spawn('rm', ['-rf', `../${repoName}`]);
-    rmProcess.on('close', (code) => {
-      if (code === 0) {
+    const kRepoURL = repoURL.replace('https://github.com', 'https://github.com');
+    console.log('repoName: ', repoName);
+    console.log('repoURL: ', kRepoURL);
+    if (!repoName) {
+      res.sendStatus(200);
+      return;
+    }
+    const repoPath = `/www/wwwroot/${repoName}`;
+    // 检查本地是否存在仓库
+    if (!fs.existsSync(repoPath)) {
+      fs.mkdirSync(repoPath);
+    }
+    // 进入repoName文件夹
+    exec(`cd /www/wwwroot/${repoName}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`cd failed with error: ${error}`);
+        res.sendStatus(200);
+        return;
+      }
+      console.log('cd successful');
+      // 需要先删除本地仓库，否则 clone 会失败
+      exec(`rm -rf /www/wwwroot/${repoName}/origin`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Delete failed with error: ${error}`);
+          res.sendStatus(200);
+          return;
+        }
         console.log('Delete successful');
         // 执行 git clone 命令
-      } else {
-        console.error(`Delete failed with code ${code}`);
-      }
-      const cloneProcess = spawn('git', [
-        'clone',
-        repoURL,
-        `../${repoName}`,
-      ]);
-      
-      cloneProcess.on('close', (code) => {
-        if (code === 0) {
+        exec(`git clone ${kRepoURL} /www/wwwroot/${repoName}/origin`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Clone failed with error: ${error}`);
+            res.sendStatus(200);
+            return;
+          }
           console.log('Clone successful');
-          // 执行其他操作，例如重启服务器、更新数据库等
-        } else {
-          console.error(`Clone failed with code ${code}`);
-        }
-        res.sendStatus(200);
+          // 将该目录下的index.html文件移动到上层
+          exec(`mv /www/wwwroot/${repoName}/origin/index.html /www/wwwroot/${repoName}/`, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`mv failed with error: ${error}`);
+              res.sendStatus(200);
+              return;
+            }
+            console.log('mv successful');
+            res.sendStatus(200);
+          })
+        });
       });
     })
+  } else {
+    res.sendStatus(200);
   }
 });
+
+app.get('/webhook_test', (req, res) => {
+  let { repoURL, } = req.query;
+  if (!repoURL) {
+    repoURL = 'https://github.com/dashjidasnda/yasa.cc.git'
+  }
+  // const kRepoURL = repoURL.replace('https://github.com', 'https://gitclone.com');
+  const kRepoURL = repoURL
+  const repoName = repoURL.split('/').pop().replace('.git', '')
+  console.log('repoName: ', repoName);
+  console.log('repoURL: ', kRepoURL);
+  console.log('path: ', kRepoURL);
+  const repoPath = `/www/wwwroot/${repoName}`;
+  exec(`cd /www/wwwroot/${repoName}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`cd failed with error: ${error}`);
+      res.sendStatus(200);
+      return;
+    }
+    console.log('cd successful');
+    // 需要先删除本地仓库，否则 clone 会失败
+    exec(`rm -rf /www/wwwroot/${repoName}/origin`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Delete failed with error: ${error}`);
+        res.sendStatus(200);
+        return;
+      }
+      console.log('Delete successful');
+      // 执行 git clone 命令
+      exec(`git clone ${kRepoURL} /www/wwwroot/${repoName}/origin`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Clone failed with error: ${error}`);
+          res.sendStatus(200);
+          return;
+        }
+        console.log('Clone successful');
+        // 将该目录下的index.html文件移动到上层
+        exec(`mv /www/wwwroot/${repoName}/origin/index.html /www/wwwroot/${repoName}/`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`mv failed with error: ${error}`);
+            res.sendStatus(200);
+            return;
+          }
+          console.log('mv successful');
+          res.sendStatus(200);
+        })
+      });
+    });
+  })
+});
+
+app.get('/test', (req, res) => {
+  res.send('test');
+})
+
 
 app.use((err, req, res, next) => {
   console.error('Error:', err);
